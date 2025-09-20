278395e8d473e985137ea1b8a5a6f3d73e69aa9e
Tarun Tyagi <tarunt@adobe.com>
2025-09-20 13:25:20 +0530

    change second

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -230,7 +230,7 @@ export default function ShopPage() {
             {[...Array(8)].map((_, i) => (
               <div key={i} className="animate-pulse">
                 <div className="bg-white rounded-lg shadow-sm p-4">
-                  <div className={viewMode === 'grid' ? 'h-48' : 'h-24'} className="bg-gray-200 rounded-lg mb-4"></div>
+                  <div   className="bg-gray-200 rounded-lg mb-4"></div>
                   <div className="h-4 bg-gray-200 rounded w-3/4 mb-2"></div>
                   <div className="h-3 bg-gray-200 rounded w-1/2"></div>
                 </div>
